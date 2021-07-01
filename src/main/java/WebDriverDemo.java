9dbe4d256f87567e060bc9858cf374f922cf67f8
Ikarashi <masayoshi.ikarashi@rakuten.com>
2021-07-01 11:53:30 +0900

    Made a PATH for windows

--- src/main/java/WebDriverDemo.java
+++ src/main/java/WebDriverDemo.java
@@ -6,7 +6,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class WebDriverDemo {
     public static void main(String[] args) {
 
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\masayoshi.ikarashi\\Workspace\\automation-test\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.google.com/");
 
