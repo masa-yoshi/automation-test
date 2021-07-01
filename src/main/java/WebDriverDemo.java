c8ad2c418875eaf3121f62bb7a271210343ebe92
Ikarashi <masayoshi.ikarashi@rakuten.com>
2021-07-01 11:31:04 +0900

    windows version

--- src/main/java/WebDriverDemo.java
+++ src/main/java/WebDriverDemo.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class WebDriverDemo {
     public static void main(String[] args) {
 
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\masayoshi.ikarashi\\Workspace\\automation-test\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.google.com/");
 
