77494dedeeaa256ea65c2e8bb611c0c4d5b221b6
ikarashi masayoshi <masayoshi@ikarashi-MacBook-Air.local>
2021-07-03 00:11:07 +0900

    using selenium server

--- src/main/java/WebDriverDemo.java
+++ src/main/java/WebDriverDemo.java
@@ -1,13 +1,22 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class WebDriverDemo {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws MalformedURLException {
 
-        WebDriver driver = new ChromeDriver();
+//        WebDriver driver = new ChromeDriver();
+
+//        WebDriver driver = new RemoteWebDriver(new URL("http://localhost:8181/wd/hub"),
+//                DesiredCapabilities.chrome());
+
+        WebDriver driver = new RemoteWebDriver(new URL("http://localhost:8181/wd/hub"),
+        new DesiredCapabilities("firefox", "", Platform.MAC));
+
         driver.get("https://www.google.com/?hl=en");
 
         WebElement searchFiled = driver.findElement(By.name("q"));
