b4aa99102c5e96d57794baa4d8ef2ab580ffe58c
ikarashi masayoshi <masayoshi@ikarashi-MacBook-Air.local>
2021-07-02 19:26:51 +0900

    add additional webelements

--- src/main/java/WebDriverDemo.java
+++ src/main/java/WebDriverDemo.java
@@ -1,4 +1,5 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -7,10 +8,16 @@ public class WebDriverDemo {
     public static void main(String[] args) {
 
         WebDriver driver = new ChromeDriver();
-        driver.get("https://www.google.com/");
+        driver.get("https://www.google.com/?hl=en");
 
         WebElement searchFiled = driver.findElement(By.name("q"));
         searchFiled.sendKeys("pluralsight");
+        searchFiled.submit();
 
+        WebElement imagesLinks = driver.findElements(By.linkText("Images")).get(0);
+        imagesLinks.click();
+
+        WebElement imageElement = driver.findElements(By.cssSelector("a[jsname = sTFXNd]")).get(0);
+        imageElement.click();
     }
 }
